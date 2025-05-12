07f270ed590bfb34602c26384c8a5cf057f60db6
xenindra <vdovenko.xenia@gmail.com>
2025-05-12 15:01:07 +0400

    Добавлена возможность запускать spleeter отдельно

--- core/infrastructure/separation/spleeter_ai.py
+++ core/infrastructure/separation/spleeter_ai.py
@@ -9,8 +9,9 @@ from core.application.dto import AudioPath
 
 
 class SpleeterSeparator(AudioSeparator):
-    def separate_into_vocals_and_music(self, audio_file: AudioPath) -> SeparationResult:
-        output_path = Path("output")  # todo
+    def separate_into_vocals_and_music(self, audio_file: AudioPath, destination_folder: Path | None = None) -> SeparationResult:
+        if destination_folder is None:
+            destination_folder = Path("output")
         separation_params = "spleeter:2stems"
         mwf = False
         adapter = "spleeter.audio.ffmpeg.FFMPEGProcessAudioAdapter"
@@ -26,7 +27,7 @@ class SpleeterSeparator(AudioSeparator):
         )
         separator.separate_to_file(
             str(audio_file),
-            str(output_path),
+            str(destination_folder or Path("output")),
             audio_adapter=audio_adapter,
             offset=offset,
             duration=duration,
@@ -38,9 +39,17 @@ class SpleeterSeparator(AudioSeparator):
         separator.join()
         return SeparationResult(
             vocals=Path(filename_format.format(
-                filename=output_path / audio_file.stem, instrument="vocals", codec=codec.value
+                filename=destination_folder / audio_file.stem, instrument="vocals", codec=codec.value
             )),
             back_track=Path(filename_format.format(
-                filename=output_path / audio_file.stem, instrument="accompaniment", codec=codec.value
+                filename=destination_folder / audio_file.stem, instrument="accompaniment", codec=codec.value
             )),
         )
+
+
+if __name__ == "__main__":
+    song_title = "Cage the elephant - Come a little closer"
+    media_folder = Path("media")
+    song_folder = media_folder / song_title
+    input_audio_file = song_folder / "audio.mp3"
+    print(SpleeterSeparator().separate_into_vocals_and_music(input_audio_file))
